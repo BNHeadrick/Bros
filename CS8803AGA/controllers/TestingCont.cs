a816f5d884fd7bd0610723f25e01983e5fa3b44f
Brandon <Hozuko@gmail.com>
2012-09-14 13:08:09 -0400

    Actually uploaded code to the Git repo
    Also added test code for getting exact x,y positions for characters
    (player).

---
+++ CS8803AGA/controllers/TestingCont.cs
@@ -0,0 +1,21 @@
+﻿using CS8803AGAGameLibrary;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using CS8803AGAGameLibrary.actions;
+using CS8803AGA.actions;
+using System.Collections.Generic;
+
+namespace CS8803AGA.controllers
+{
+    
+
+    /// <summary>
+    /// Class for playing back Animations stored in an AnimationSet.
+    /// </summary>
+    public class TestingCont
+    {
+        
+
+    }
+}
